392f429a606ae06e61dbd303ab229e345d76d252
solomonc123 <63976944+solomonc123@users.noreply.github.com>
2023-03-27 12:48:32 -0400

    [sjc2233] update shell script

--- setup.sh
+++ setup.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
 echo -e "\e[31mINSTALLING E6111 VM SETUP SOFTWARE\e[0m"
-
-# Update package lists and install required software
-sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install -y python3-pip python3.7 python3.7-dev
-
-# Upgrade pip and install google-api-python-client
-python3 -m pip install --upgrade pip
-pip3 install --upgrade google-api-python-client
+sudo apt-get -y update
+sudo apt-get install python3-pip
+python3 -m pip install pip --upgrade
+sudo apt-get install python3.7
+sudo apt-get install python3.7-dev
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 1
+sudo pip3 install --upgrade google-api-python-client
 
 echo -e "\e[31mINSTALLING PROJECT SPECIFIC SOFTWARE\e[0m"
 
-# Install beautifulsoup4, spacy, and spacy's en_core_web_lg
+sudo apt update
+sudo apt install python3-pip
 pip3 install beautifulsoup4
+sudo apt-get update
 pip3 install -U pip setuptools wheel
 pip3 install -U spacy
-python3 -m spacy download en_core_web_lg
-
-# Install packages from requirements.txt, download finetuned model, and install openai
+python3 -m spacy download en_core_web_lg 
 pip3 install -r requirements.txt
 bash download_finetuned.sh
 pip3 install openai
